b86c559f74eefdd714b5c463458e23463368be28
bell-ho <62537935+bell-ho@users.noreply.github.com>
2021-04-21 17:36:53 +0900

    프로젝트 디비 연결테스트

--- src/test/org/zerock/persistence/JDBCTests.java
+++ src/test/org/zerock/persistence/JDBCTests.java
@@ -1,9 +1,8 @@
-package org.zerock.sample.zerock.sample;
+package org.zerock.persistence;
 
 import lombok.extern.log4j.Log4j;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +11,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-
+@Log4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-@Log4j
-public class SampleTests {
+public class JDBCTests {
 
     @Autowired
     private DataSource dataSource;
@@ -28,21 +25,42 @@ public class SampleTests {
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
 
-    @Ignore
-    @Test
-    public void testConnection() {
-        try (Connection con = dataSource.getConnection()) {
-            log.info(con);
-        } catch (Exception e) {
-            fail("실패 : " + e.getMessage());
+    static {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
     @Test
-    public void testMyBatis() {
+    public void testConne() {
+        try (Connection con = DriverManager.getConnection(
+                "jdbc:log4jdbc:mariadb://sts-project.ckrxh8kwbp9e.ap-northeast-2.rds.amazonaws.com:3306/sts_project",
+                "sts",
+                "managermanager"
+        ))
+        {
+            log.info(con);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testC(){
+        try (Connection con = dataSource.getConnection()){
+            log.info(con);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testBa(){
         try (SqlSession session = sqlSessionFactory.openSession();
              Connection con = session.getConnection();
-        ) {
+             ){
             log.info(session);
             log.info(con);
         } catch (SQLException throwables) {
