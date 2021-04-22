01139a728713280d75d308a8d00969c3e81f5031
bell-ho <62537935+bell-ho@users.noreply.github.com>
2021-04-22 17:19:38 +0900

    프로젝트 컨트롤러

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
 @Controller
@@ -21,5 +24,34 @@ public class BoardController {
         log.info("게시판 리스트");
         model.addAttribute("list", boardService.getList());
         return "/board/list";
+    }
+
+    @GetMapping("/boardInser")
+    public String register() {
+        return "";
+    }
+
+    @PostMapping("/boardInsert")
+    public String register(BoardVO boardVO, RedirectAttributes rttr) {
+        boardService.insertKey(boardVO);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/boardRead")
+    public String get(BoardVO boardVO, Model model) {
+        model.addAttribute("board", boardService.read(boardVO));
+        return "/board/read";
+    }
+
+    @PostMapping("/boardUpdate")
+    public String update(BoardVO boardVO, RedirectAttributes rttr) {
+        boardService.update(boardVO);
+        return "redirect:/board/list";
+    }
+
+    @PostMapping("/boardDelete")
+    public String delete(BoardVO boardVO) {
+        boardService.delete(boardVO);
+        return "redirect:/board/list";
     }
 }
